d786081e15d53da1f9ba9befb48c5b349412158b
Kelder Passos <kelder.passos@gmail.com>
2022-07-17 19:44:00 -0300

    Tests: firsts tests being implemented

---
+++ src/tests/Favorites.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderWithRouter from './helpers/renderWithRouter';
+import { describe, expect, it } from 'vitest';
+import App from '../App';
+import { screen } from '@testing-library/react';
+
+describe('Tests the page "/favorites"', () => {
+  it('should be empty on first render', () => {
+
+  });
+
+})
