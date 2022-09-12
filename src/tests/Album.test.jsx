daf39a8425351d627844046a6d0c5e443e3b25f1
Kelder Passos <kelder.passos@gmail.com>
2022-09-11 22:02:23 -0300

    page Album 100% covered

--- src/tests/Album.test.jsx
+++ src/tests/Album.test.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import renderWithRouter from './helpers/renderWithRouter';
-import getMusics from '../services/musicsAPI';
 import App from '../App';
+import data from './helpers/dataMock';
 
-
-
-
-describe('Test the page album', () => {
-  // afterEach(() => {
-  //   vi.restoreAllMocks()
-  // });
-  
-  const ID = '1299151419';
-  // vi.mock('../services/musicsAPI');
+describe('Test the page album', () => {  
+  const ID = '349769419';
 
   
   it('should render the selected album', async () => {
-    // renderWithRouter(<App />, { route: `/album/${ID}` });
     
-    // const getMusics = vi.fn();
-    // screen.debug();
+    global.fetch = vi.fn(() => Promise.resolve({
+      json: () => Promise.resolve({
+        results: [data],
+      }),
+    }));
 
-  })
+    renderWithRouter(<App />, { route: `/album/${ID}` });
+
+    const loadingMessage = await screen.findByText('Loading...');
+    expect(loadingMessage).toBeInTheDocument();
+
+    const albumNameEl = screen.getByRole('heading', { name: 'Chico Buarque de Hollanda' });
+    expect(albumNameEl).toBeDefined();
+  });
 })
